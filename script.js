22031f286ef16ee5866a2367b301b951cdac2d06
Contrarist <104249079+hacatker@users.noreply.github.com>
2023-07-02 23:18:57 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,306 @@
+var dictionary1 = {
+    'あ': 'a',
+    'い': 'i',
+    'う': 'u',
+    'え': 'e',
+    'お': 'o',
+    'か': 'ka',
+    'き': 'ki',
+    'く': 'ku',
+    'け': 'ke',
+    'こ': 'ko',
+    'さ': 'sa',
+    'し': 'shi',
+    'す': 'su',
+    'せ': 'se',
+    'そ': 'so',
+    'た': 'ta',
+    'ち': 'chi',
+    'つ': 'tsu',
+    'て': 'te',
+    'と': 'to',
+    'な': 'na',
+    'に': 'ni',
+    'ぬ': 'nu',
+    'ね': 'ne',
+    'の': 'no',
+    'は': 'ha',
+    'ひ': 'hi',
+    'ふ': 'fu',
+    'へ': 'he',
+    'ほ': 'ho',
+    'ま': 'ma',
+    'み': 'mi',
+    'む': 'mu',
+    'め': 'me',
+    'も': 'mo',
+    'や': 'ya',
+    'ゆ': 'yu',
+    'よ': 'yo',
+    'ら': 'ra',
+    'り': 'ri',
+    'る': 'ru',
+    'れ': 're',
+    'ろ': 'ro',
+    'わ': 'wa',
+    'を': 'wo',
+    'ん': 'n',
+    'が': 'ga',
+    'ぎ': 'gi',
+    'ぐ': 'gu',
+    'げ': 'ge',
+    'ご': 'go',
+    'ざ': 'za',
+    'じ': 'ji',
+    'ず': 'zu',
+    'ぜ': 'ze',
+    'ぞ': 'zo',
+    'だ': 'da',
+    'ぢ': 'ji',
+    'づ': 'zu',
+    'で': 'de',
+    'ど': 'do',
+    'ば': 'ba',
+    'び': 'bi',
+    'ぶ': 'bu',
+    'べ': 'be',
+    'ぼ': 'bo',
+    'ぱ': 'pa',
+    'ぴ': 'pi',
+    'ぷ': 'pu',
+    'ぺ': 'pe',
+    'ぽ': 'po'
+};
+
+var dictionary11 = {
+    'きゃ': 'kya',
+    'きゅ': 'kyu',
+    'きょ': 'kyo',
+    'しゃ': 'sha',
+    'しゅ': 'shu',
+    'しょ': 'sho',
+    'ちゃ': 'cha',
+    'ちゅ': 'chu',
+    'ちょ': 'cho',
+    'にゃ': 'nya',
+    'にゅ': 'nyu',
+    'にょ': 'nyo',
+    'ひゃ': 'hya',
+    'ひゅ': 'hyu',
+    'ひょ': 'hyo',
+    'みゃ': 'mya',
+    'みゅ': 'myu',
+    'みょ': 'myo',
+    'りゃ': 'rya',
+    'りゅ': 'ryu',
+    'りょ': 'ryo',
+    'ぎゃ': 'gya',
+    'ぎゅ': 'gyu',
+    'ぎょ': 'gyo',
+    'じゃ': 'ja',
+    'じゅ': 'ju',
+    'じょ': 'jo',
+    'びゃ': 'bya',
+    'びゅ': 'byu',
+    'びょ': 'byo',
+    'ぴゃ': 'pya',
+    'ぴゅ': 'pyu',
+    'ぴょ': 'pyo'
+};
+
+var dictionary2 = {
+    'ア': 'a',
+    'イ': 'i',
+    'ウ': 'u',
+    'エ': 'e',
+    'オ': 'o',
+    'カ': 'ka',
+    'キ': 'ki',
+    'ク': 'ku',
+    'ケ': 'ke',
+    'コ': 'ko',
+    'サ': 'sa',
+    'シ': 'shi',
+    'ス': 'su',
+    'セ': 'se',
+    'ソ': 'so',
+    'タ': 'ta',
+    'チ': 'chi',
+    'ツ': 'tsu',
+    'テ': 'te',
+    'ト': 'to',
+    'ナ': 'na',
+    'ニ': 'ni',
+    'ヌ': 'nu',
+    'ネ': 'ne',
+    'ノ': 'no',
+    'ハ': 'ha',
+    'ヒ': 'hi',
+    'フ': 'fu',
+    'ヘ': 'he',
+    'ホ': 'ho',
+    'マ': 'ma',
+    'ミ': 'mi',
+    'ム': 'mu',
+    'メ': 'me',
+    'モ': 'mo',
+    'ヤ': 'ya',
+    'ユ': 'yu',
+    'ヨ': 'yo',
+    'ラ': 'ra',
+    'リ': 'ri',
+    'ル': 'ru',
+    'レ': 're',
+    'ロ': 'ro',
+    'ワ': 'wa',
+    'ヲ': 'wo',
+    'ン': 'n',
+    'ガ': 'ga',
+    'ギ': 'gi',
+    'グ': 'gu',
+    'ゲ': 'ge',
+    'ゴ': 'go',
+    'ザ': 'za',
+    'ジ': 'ji',
+    'ズ': 'zu',
+    'ゼ': 'ze',
+    'ゾ': 'zo',
+    'ダ': 'da',
+    'ヂ': 'ji',
+    'ヅ': 'zu',
+    'デ': 'de',
+    'ド': 'do',
+    'バ': 'ba',
+    'ビ': 'bi',
+    'ブ': 'bu',
+    'ベ': 'be',
+    'ボ': 'bo',
+    'パ': 'pa',
+    'ピ': 'pi',
+    'プ': 'pu',
+    'ペ': 'pe',
+    'ポ': 'po'
+};
+
+var dictionary22 = {
+    'キャ': 'kya',
+    'キュ': 'kyu',
+    'キョ': 'kyo',
+    'シャ': 'sha',
+    'シュ': 'shu',
+    'ショ': 'sho',
+    'チャ': 'cha',
+    'チュ': 'chu',
+    'チョ': 'cho',
+    'ニャ': 'nya',
+    'ニュ': 'nyu',
+    'ニョ': 'nyo',
+    'ヒャ': 'hya',
+    'ヒュ': 'hyu',
+    'ヒョ': 'hyo',
+    'ミャ': 'mya',
+    'ミュ': 'myu',
+    'ミョ': 'myo',
+    'リャ': 'rya',
+    'リュ': 'ryu',
+    'リョ': 'ryo',
+    'ギャ': 'gya',
+    'ギュ': 'gyu',
+    'ギョ': 'gyo',
+    'ジャ': 'ja',
+    'ジュ': 'ju',
+    'ジョ': 'jo',
+    'ビャ': 'bya',
+    'ビュ': 'byu',
+    'ビョ': 'byo',
+    'ピャ': 'pya',
+    'ピュ': 'pyu',
+    'ピョ': 'pyo'
+};
+
+function randomItem(dictionary) {
+    var keys = Object.keys(dictionary);
+    var randomKey = keys[Math.floor(Math.random() * keys.length)];
+    return { kana: randomKey, romaji: dictionary[randomKey] };
+}
+
+function getWeights() {
+    var hiraganaWeight = parseInt(document.getElementById('hiragana-weight').value);
+    var hiraganaYouonWeight = parseInt(document.getElementById('hiragana-youon-weight').value);
+    var katakanaWeight = parseInt(document.getElementById('katakana-weight').value);
+    var katakanaYouonWeight = parseInt(document.getElementById('katakana-youon-weight').value);
+    
+    return {
+        Hiragana: hiraganaWeight,
+        Hiragana_youon: hiraganaYouonWeight,
+        Katakana: katakanaWeight,
+        Katakana_youon: katakanaYouonWeight
+    };
+}
+
+function renderKana() {
+    var item;
+    var weights = getWeights();
+    
+    var case1 = weights.Hiragana;
+    var case2 = case1 + weights.Hiragana_youon;
+    var case3 = case2 + weights.Katakana;
+    var case4 = case3 + weights.Katakana_youon;
+    
+    var term = Math.floor(Math.random() * (weights.Hiragana + weights.Hiragana_youon + weights.Katakana + weights.Katakana_youon)) + 1;
+    
+    if (term < case1) {
+        item = randomItem(dictionary1);
+    } else if (term >= case1 && term < case2) {
+        item = randomItem(dictionary11);
+    } else if (term >= case2 && term < case3) {
+        item = randomItem(dictionary2);
+    } else if (term >= case3) {
+        item = randomItem(dictionary22);
+    }
+
+    document.getElementById('kana').innerText = item.kana;
+    document.getElementById('answer').value = '';
+    document.getElementById('result').innerText = '';
+}
+
+function checkAnswer() {
+    var kana = document.getElementById('kana').innerText;
+    var answer = document.getElementById('answer').value.trim();
+    var dictionary;
+
+    if (dictionary1.hasOwnProperty(kana)) {
+        dictionary = dictionary1;
+    } else if (dictionary11.hasOwnProperty(kana)) {
+        dictionary = dictionary11;
+    } else if (dictionary2.hasOwnProperty(kana)) {
+        dictionary = dictionary2;
+    } else {
+        dictionary = dictionary22;
+    }
+
+    if (dictionary[kana].toLowerCase() === answer.toLowerCase()) {
+        document.getElementById('result').innerText = '正确！';
+        setTimeout(renderKana, 630);
+    } else {
+        var time = 1800
+        var wrongKana = Object.keys(dictionary).find(key => dictionary[key].toLowerCase() === answer.toLowerCase());
+        if (wrongKana) {
+            document.getElementById('result').innerText = '你输入的假名是：' + wrongKana;
+            time += 1700;
+        }
+        document.getElementById('kana').innerText +=  " : " + dictionary[kana];
+        setTimeout(renderKana, time);
+    }
+}
+
+function checkAnswerOnEnter(event) {
+    if (event.keyCode === 13) {
+        checkAnswer();
+    }
+}
+
+window.onload = function() {
+    renderKana();
+    document.getElementById('answer').addEventListener('keydown', checkAnswerOnEnter);
+};
